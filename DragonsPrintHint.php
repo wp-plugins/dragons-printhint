0ec063a9dd1b28090d7a4b5344e33d967c41eb73
Kronester <Kronester@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-28 22:10:18 +0000

    New deployment but css folder lost in 0.3
    git-svn-id: https://plugins.svn.wordpress.org/dragons-printhint/tags/0.3.1@130280 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ DragonsPrintHint.php
@@ -0,0 +1,258 @@
+<?php
+/*
+Plugin Name: Dragons Print-Hint
+Plugin URI: http://www.kroni.de
+Description: Einblenden eines Hinweis-Textes beim Ausdrucken.
+Version: 0.3.1
+Author: Roy Kronester
+Author URI: http://www.kronester.com
+*/
+
+/*  Copyright 2009  Roy Kronester  (email : Roy@Kronester.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+$fdrag_phi_hinttext  = 'Hello world!';
+$fdrag_phi_removecss = '';
+
+function fdrag_phi_Header()
+{
+	global $fdrag_phi_removecss;
+	
+	$StylePath = get_bloginfo("wpurl") . '/wp-content/plugins/DragonsPrintHint/css/';
+
+	echo '<link rel="stylesheet" type="text/css" href="'.$StylePath.'fdrag_phi_print.css" media="print">';
+	echo '<link rel="stylesheet" type="text/css" href="'.$StylePath.'fdrag_phi_screen.css" media="screen">';
+
+	$opt_removecss = "fdrag_phi_removecss";
+
+	if (get_option($opt_removecss))
+	{
+		$fdrag_phi_removecss 	= get_option($opt_removecss);
+		
+		if (strlen($fdrag_phi_removecss)>2)
+		{
+		   echo '<style type="text/css" media="print">'. $fdrag_phi_removecss .'{display:none;}</style>';	
+		}
+	}
+	else
+	{
+		add_option($opt_removecss,'','Entfernt CSS-Blöcke (Kommagetrennte Liste)','no');
+	}
+}
+
+function fdrag_phi_ImportStyleSheet()
+{
+	$StyleSheet = WP_PLUGIN_URL . '/DragonsPrintHint/css/fdrag_phi_AdminPrintHint.css';
+	
+    wp_register_style('DragonHintPrintStyle', $StyleSheet);
+    wp_enqueue_style( 'DragonHintPrintStyle');
+}
+
+function fdrag_phi_menu() 
+{
+  	add_submenu_page('options-general.php', 'Dragons PrintHint', 'Dragons Print-Hint', 2, __FILE__, fdrag_phi_dashboard); 	
+}
+
+function fdrag_phi_dashboard() 
+{
+	global $fdrag_phi_hinttext;
+	
+	echo "<h2>Dragon's Print-Hints</h2>";
+	
+	// Variablen verarbeiten 
+	
+	fdrag_phi_GetVariables();
+	
+	// Submit-Buttons verarbeiten
+	
+	fdrag_phi_ProcessSubmits();
+	
+	// Maske aufbauen
+	
+  	fdrag_phi_Div_Eingabe();
+	
+	// Variablen sichern
+	
+	fdrag_phi_SaveVariables();
+
+	// Donation einsetzen
+	
+	fdrag_phi_donate();
+}
+	
+// ------------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------------
+
+function fdrag_phi_donate()
+{
+	echo '<div class="fdrag_phi_donate">
+		<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+		<input type="hidden" name="cmd" value="_s-xclick">
+		<input type="hidden" name="hosted_button_id" value="6387192">
+		<input type="image" src="https://www.paypal.com/de_DE/DE/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="Jetzt einfach, schnell und sicher online bezahlen – mit PayPal.">
+		<img alt="" border="0" src="https://www.paypal.com/de_DE/i/scr/pixel.gif" width="1" height="1">
+		</form>
+		<div>
+		';
+}
+
+function fdrag_phi_ProcessSubmits()
+{
+	global $fdrag_phi_hinttext;
+	global $fdrag_phi_removecss;
+	
+	// ---------------------------------------------------------------------------------------------------------------------------------
+	// Verarbeitung der Daten
+	// ---------------------------------------------------------------------------------------------------------------------------------
+		
+	if ($_POST) 
+	{
+		if ($_POST['btn_savehint'])
+		{	
+			$fdrag_phi_hinttext = htmlspecialchars($_POST['HintText']);
+			$fdrag_phi_removecss= htmlspecialchars($_POST['RemoveCssWhilePrinting']);
+		}
+	}
+}
+	
+function fdrag_phi_GetVariables()
+{
+	global $current_user;
+	global $fdrag_phi_hinttext;
+	global $fdrag_phi_removecss;
+	
+  	get_currentuserinfo();
+
+	if ($current_user->ID = '')
+	{
+		http_redirect("/wp-login.php", NULL, true, HTTP_REDIRECT_PERM);
+	}
+
+	$opt_hinttext  = "fdrag_phi_hinttext";
+	$opt_removecss = "fdrag_phi_removecss";
+
+	if (get_option($opt_removecss))
+	{
+		$fdrag_phi_removecss = get_option($opt_removecss);
+	}
+	else
+	{
+		add_option($opt_removecss,'','Entfernt CSS-Blöcke (Kommagetrennte Liste)','no');
+	}
+	
+	if (get_option($opt_hinttext))
+	{
+		$fdrag_phi_hinttext = get_option($opt_hinttext);
+	}
+	else
+	{
+		add_option($opt_hinttext,'','Hinweis-Text für Druckausgabe (DragonsPrintHint)','no');
+	}
+	
+//	print 'A:'.$fdrag_phi_removecss;
+}
+	
+function fdrag_phi_SaveVariables()
+{
+	global $current_user;
+	global $fdrag_phi_hinttext;
+	global $fdrag_phi_removecss;
+	
+  	get_currentuserinfo();
+
+	if ($current_user->ID = '')
+	{
+		http_redirect("/wp-login.php", NULL, true, HTTP_REDIRECT_PERM);
+	}
+	
+	$opt_hinttext  = "fdrag_phi_hinttext";
+	$opt_removecss = "fdrag_phi_removecss"; 
+
+	update_option($opt_hinttext,$fdrag_phi_hinttext);
+	update_option($opt_removecss,$fdrag_phi_removecss);
+	
+//	print 'B:'.$fdrag_phi_removecss;
+}
+
+function fdrag_phi_Div_Eingabe()
+{
+	global $fdrag_phi_hinttext;
+	global $fdrag_phi_removecss;
+	
+	echo '
+		<div class="fdrag_phi_Input">
+			<form action="" method="post">
+				<ul type="none" id="fdrag_phi_Input_Col2">
+				    <li>
+						<label     for="HintText">Hint - Text:</label>
+						<textarea name="HintText"             type="text" id="hinttext"  cols="80" rows="5" class="regular-text code">' . $fdrag_phi_hinttext . '</textarea></li>
+					<li>
+						<label     for="RemoveCssWhilePrinting">Hide CSS elements while printing:</label>
+						<textarea name="RemoveCssWhilePrinting" type="text" id="removecss" cols="80" rows="5" class="regular-text code">' . $fdrag_phi_removecss . '</textarea></li>
+				</ul>
+				
+				<!-- Submit-Button für Login-Daten -->
+				<ul type="none" id="fdrag_phi_Input_Footer"><li><input type="submit" name="btn_savehint" id="btn_savehint" class="button-primary" value="Speichern" /></li></ul>
+			</form>
+		</div>
+	';
+}
+
+function fdrag_phi_PrintHintFilter($text)
+{
+	global $fdrag_phi_hinttext;
+	
+	fdrag_phi_GetVariables();
+	
+	$HintText = '<div class="fdrag_phi_JustPrint"><p>'.htmlspecialchars_decode($fdrag_phi_hinttext).'</p></div>';
+	
+	$HintText = $HintText;
+	
+	$text = $HintText . $text;
+	return $text;
+}
+
+function fdrag_phi_RemovePrintHint($text)
+{
+	global $fdrag_phi_hinttext;
+	
+	fdrag_phi_GetVariables();
+	
+	$HintText = strip_tags(htmlspecialchars_decode($fdrag_phi_hinttext));
+	$Replace  = '';
+	
+	if (preg_match('/.*$/', $HintText, $Ergebnis))
+	{
+		$text = substr($text,strpos($text,$Ergebnis[0])+strlen($Ergebnis[0]));
+	}
+				
+	return $text;
+}
+
+
+remove_action("admin_print_styles", fdrag_phi_ImportStyleSheet);
+remove_action("wp_head"	          , fdrag_phi_Header);
+remove_action('admin_menu'        , fdrag_phi_menu  );
+
+add_action('admin_menu' ,'fdrag_phi_menu'  );
+add_action("admin_print_styles", fdrag_phi_ImportStyleSheet    );
+add_action('wp_head'	       , fdrag_phi_Header           ,10);
+
+add_filter('the_content','fdrag_phi_PrintHintFilter',1);
+add_filter('the_excerpt','fdrag_phi_RemovePrintHint',1);
+
+?>
---
+++ readme.txt
@@ -0,0 +1,71 @@
+=== Plugin Name ===
+Contributors: Roy Kronester
+Donate link: http://www.kronester.com
+Tags: print, comment, license
+Requires at least: 2.7.1
+Tested up to: 2.8
+Stable tag: 0.3.1
+
+The plugin prints a hint text
+
+== Description ==
+
+There are several reasons to print some hints on your posts. These hints will never been seen on the screen, but you want it to show on paper.
+You can customize the hint text within the settings of the administration panel.
+
+The plugin hooks into *the_content* filter. The *the_excerpt* filter hook is used to remove the hint from displaying on search result page.
+
+You can use any html tag that can be placed within `<p> ... </p>` (a paragraph).
+
+Here is an example (just in german) that demonstrate the usage ob html tags within the hint text.
+Tip: The [...] are not part of the hint.
+
+[begin comment]
+<center><b>Achtung</b> - Urheberrechtshinweis! <br>
+Da dieses Blog unter einer Creative Commons Lizenz steht, 
+stellt das Ausdrucken für Sie kein Problem dar. <br>
+Bei Webseiten die nicht unter einer solchen oder ähnlichen Lizenz stehen, 
+kann das Ausdrucken zu einer Urheberrechtsverletzung führen.</center>
+[end comment]
+
+In addition you can set now some (comma delimited) CSS classes / idtags to hide while printing.
+If you do this, a <style>-Element is inserted to the header of every page, defining the defined list to be display:none;
+
+== Installation ==
+
+1. Unzip `DragonsPrintHint.zip` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Customize the hint text in admin panel settings menu
+4. Verify your text within the print preview of your browser
+
+== Frequently Asked Questions ==
+
+= What kind of CSS classes / ids can i hide? =
+
+You can insert all CSS classes / ids into the input box. These comma separated list
+will be inserted in a style tag with display:none;
+Be aware that doing some experiments without using your brain can hide the whole page while printing!
+
+== Screenshots ==
+
+1. Customize your hint text
+2. Sample text shown in print preview
+
+== Changelog ==
+
+= 0.2 =
+* Remove hint text in search results. 
+* therefore there is no hint if you print the results!
+
+= 0.3 =
+* Changed the file structure (css moved to subfolder)
+* Added functionality for hiding CSS classes / ids while printing
+
+= 0.3.1 0
+
+CSS subfolder was not deployed with 0.3. Generating new version with css subfolder.
+
+== Arbitrary section ==
+
+
+
